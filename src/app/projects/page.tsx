d328323482e50f13e5ae424f56252332d87a5f2d
naannddaa <nandakalipait@gmail.com>
2026-05-30 14:01:49 +0700

    page tsx add code

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ProjectsPage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Daftar Project</h1>
+      <p>Halaman ini sedang dalam pengembangan.</p>
+    </div>
+  );
+}
